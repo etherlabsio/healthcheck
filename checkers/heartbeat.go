097a30510dd80c85dbcb4b8b14c0fb7409c894a8
Karthik Muralidharan <mkarthik91@gmail.com>
2017-08-18 23:28:03 +0530

    add; healthcheck endpoint

---
+++ checkers/heartbeat.go
@@ -0,0 +1,41 @@
+package checkers
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+
+	health "github.com/etherlabsio/healthcheck"
+)
+
+type heartbeat struct {
+	path string
+}
+
+func (h *heartbeat) Check() error {
+	if _, err := os.Stat(h.path); err != nil {
+		return errors.New("heartbeat not found. service could be taken out of rotation")
+	}
+	return nil
+}
+
+// Heartbeat returns a heartbeat health checker. Heartbeat files are generally used to take hosts out of rotation from the loadbalancers.
+// Removing the heartbeat file allows you to debug the application host in case of failures.
+func Heartbeat(filepath string) health.Checker {
+	return &heartbeat{absFilePath(filepath)}
+}
+
+func absFilePath(inPath string) string {
+	if strings.HasPrefix(inPath, "$") {
+		end := strings.Index(inPath, string(os.PathSeparator))
+		inPath = os.Getenv(inPath[1:end]) + inPath[end:]
+	}
+	if filepath.IsAbs(inPath) {
+		return filepath.Clean(inPath)
+	}
+	if p, err := filepath.Abs(inPath); err == nil {
+		return filepath.Clean(p)
+	}
+	return ""
+}
---
+++ health.go
@@ -0,0 +1,68 @@
+package health
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type (
+	// Option adds optional parameter for the HealthcheckHandlerFunc
+	Option func(*health)
+
+	// Checker checks the status of the dependency and returns error.
+	// In case the dependency is working as expected, return nil.
+	Checker interface {
+		Check() error
+	}
+
+	health struct {
+		checkers map[string]Checker
+	}
+)
+
+func (h *health) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	code := http.StatusOK
+	var errorMsgs map[string]string
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	for key, checker := range h.checkers {
+		if err := checker.Check(); err != nil {
+			errorMsgs[key] = err.Error()
+			code = http.StatusServiceUnavailable
+		}
+	}
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(struct {
+		Status string            `json:"status,omitempty"`
+		Errors map[string]string `json:"errors,omitempty"`
+	}{
+		Status: http.StatusText(code),
+		Errors: errorMsgs,
+	})
+}
+
+func new(opts ...Option) *health {
+	h := &health{
+		checkers: make(map[string]Checker),
+	}
+	for _, opt := range opts {
+		opt(h)
+	}
+	return h
+}
+
+// NewHandlerFunc returns a http.Handler
+func NewHandlerFunc(opts ...Option) http.Handler {
+	return new(opts...)
+}
+
+// NewHandler returns a http.Handler
+func NewHandler(opts ...Option) http.HandlerFunc {
+	return new(opts...).ServeHTTP
+}
+
+// WithChecker adds a status checker that needs to be added as part of healthcheck. i.e database, cache or any external dependency
+func WithChecker(name string, s Checker) Option {
+	return func(h *health) {
+		h.checkers[name] = s
+	}
+}
